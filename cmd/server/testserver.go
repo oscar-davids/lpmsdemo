47b9474ffed43121c54560d1a3975a0c78466698
User01 <woody_0105@outlook.com>
2020-06-22 20:23:03 +0800

    modified:   cmd/server/testserver.go 	modified:   detection/detection.go

--- cmd/server/testserver.go
+++ cmd/server/testserver.go
@@ -1,135 +1,215 @@
 package main
 
 import (
+	"bytes"
+	"encoding/hex"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
+	"net/url"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/golang/glog"
 
 	"github.com/oscar-davids/lpmsdemo/detection"
 	"github.com/oscar-davids/lpmsdemo/ffmpeg"
+	"github.com/oscar-davids/lpmsdemo/stream"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-type User struct {
-	gorm.Model
-	Name  string
-	Email string
-}
-
-type UserRequest struct {
-	Name  string `json: "name"`
-	Email string `json: "email"`
-}
-
-type ProfileRequest struct {
-	Name        string
-	Bitrate     string
-	Fps         uint
-	Resolution  string
-	AspectRatio string
-	Detector    string
-}
-
 type StreamRequest struct {
 	Name     string
-	Profiles []ProfileRequest
+	Profiles []ffmpeg.VideoProfile
 }
 
-var videoprofiles []ffmpeg.VideoProfile
-
-func allUsers(w http.ResponseWriter, r *http.Request) {
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-
-	var users []User
-	db.Find(&users)
-	fmt.Println("{}", users)
-
-	json.NewEncoder(w).Encode(users)
+type BroadcasterAddress struct {
+	Address string `json:"address"`
 }
 
-func newUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("New User Endpoint Hit")
-	w.Header().Set("Content-Type", "application/json")
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-	var userRequest UserRequest
-	json.NewDecoder(r.Body).Decode(&userRequest)
-	fmt.Println(userRequest.Name)
-	fmt.Println(userRequest.Email)
+var transcodeprofiles []ffmpeg.VideoProfile
+var broadcaster string
+var broadcastercfg string
 
-	db.Create(&User{Name: userRequest.Name, Email: userRequest.Email})
-	fmt.Fprintf(w, "New User Successfully Created")
+// RandomIDGenerator generates random hexadecimal string of specified length
+// defined as variable for unit tests
+var RandomIDGenerator = func(length uint) string {
+	x := make([]byte, length, length)
+	for i := 0; i < len(x); i++ {
+		x[i] = byte(rand.Uint32())
+	}
+	return hex.EncodeToString(x)
+}
+
+func RandName() string {
+	return RandomIDGenerator(10)
+}
+
+func getBroadcaster(w http.ResponseWriter, r *http.Request) {
+	var broadcasteraddress BroadcasterAddress
+	if len(broadcaster) > 0 {
+		broadcasteraddress = BroadcasterAddress{Address: broadcaster}
+	}
+
+	json.NewEncoder(w).Encode(broadcasteraddress)
 }
 
 func newStream(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("New Stream Endpoint Hit")
 	w.Header().Set("Content-Type", "application/json")
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
+
 	var streamRequest StreamRequest
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(reqBody, &streamRequest)
 
 	fmt.Println(streamRequest.Name)
-	var profiles []ProfileRequest
+	var profiles []ffmpeg.VideoProfile
 	profiles = streamRequest.Profiles
 
-	// fmt.Println(profiles)
-	// fmt.Println(profiles[0].Name, profiles[0].Bitrate, profiles[0].Detector)
+	var detectionprofiles []ffmpeg.VideoProfile
 
 	for _, profile := range profiles {
-		videoprofile := ffmpeg.VideoProfileLookup[profile.Name]
-		if len(profile.Detector) > 0 {
-			fmt.Println("adding video profile detector:", profile.Detector)
-			videoprofile.Detector = ffmpeg.DetectorProfileLookup[profile.Detector]
+		if strings.Index(profile.Name, "PDnn") < 0 {
+			videoprofile := ffmpeg.VideoProfileLookup[profile.Name]
+			transcodeprofiles = append(transcodeprofiles, videoprofile)
+		} else {
+			detectionprofile := ffmpeg.VideoProfileLookup[profile.Name]
+			detectionprofiles = append(detectionprofiles, detectionprofile)
 		}
-		videoprofiles = append(videoprofiles, videoprofile)
+
 	}
 
-	detection.CreateVideoProfile(videoprofiles)
+	detection.InitDnnEngine(detectionprofiles)
+
+	broadcaster = broadcastercfg
 
 	fmt.Fprintf(w, "New Stream Successfully Created")
 }
 
+func handlePush(w http.ResponseWriter, r *http.Request) {
+	// we read this unconditionally, mostly for ffmpeg
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		httpErr := fmt.Sprintf(`Error reading http request body: %s`, err.Error())
+		glog.Error(httpErr)
+		http.Error(w, httpErr, http.StatusInternalServerError)
+		return
+	}
+
+	r.URL = &url.URL{Scheme: "http", Host: r.Host, Path: r.URL.Path}
+	vars := mux.Vars(r)
+	stream_id := vars["stream_id"]
+
+	fname := path.Base(r.URL.Path)
+	seq, err := strconv.ParseUint(strings.TrimSuffix(fname, ".ts"), 10, 64)
+	if err != nil {
+		seq = 0
+	}
+
+	duration, err := strconv.Atoi(r.Header.Get("Content-Duration"))
+	if err != nil {
+		duration = 2000
+		glog.Info("Missing duration; filling in a default of 2000ms")
+	}
+
+	// write ts file to tmp directory
+	workDir := ".tmp/"
+	file, err := os.Create(workDir + fname)
+	if err != nil {
+		panic(err)
+	}
+	file.Write(body)
+	file.Close()
+	r.Body.Close()
+
+	seg := &stream.HLSSegment{
+		Data:     body,
+		Name:     fname,
+		SeqNo:    seq,
+		Duration: float64(duration) / 1000.0,
+	}
+
+	// check if stream with transcoding profiles is created
+	if len(transcodeprofiles) <= 0 {
+		http.Error(w, "No Video Profile Created. Create video profile first and try again.", http.StatusInternalServerError)
+		return
+	}
+
+	// transcode and detect
+	resultdata, contents, err := detection.ProcessSegment(seg, stream_id, transcodeprofiles)
+	if err != nil {
+		glog.Errorf("Error transcoding: %v", err)
+	} else {
+		fmt.Println("Contents Detected:", contents)
+	}
+
+	os.Remove(workDir + fname)
+
+	boundary := RandName()
+	accept := r.Header.Get("Accept")
+	if accept == "multipart/mixed" {
+		contentType := "multipart/mixed; boundary=" + boundary
+		w.Header().Set("Content-Type", contentType)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.(http.Flusher).Flush()
+	if accept != "multipart/mixed" {
+		return
+	}
+	mw := multipart.NewWriter(w)
+
+	for i, result := range resultdata {
+		mw.SetBoundary(boundary)
+		var typ, ext string
+		length := len(result)
+		ext = ".ts"
+
+		typ = "video/mp2t"
+
+		profile := transcodeprofiles[i].Name
+		fname := fmt.Sprintf(`"%s_%d%s"`, profile, seq, ext)
+		hdrs := textproto.MIMEHeader{
+			"Content-Type":        {typ + "; name=" + fname},
+			"Content-Length":      {strconv.Itoa(length)},
+			"Content-Disposition": {"attachment; filename=" + fname},
+			"Rendition-Name":      {profile},
+			"Detection-Result":    {contents},
+		}
+		fw, err := mw.CreatePart(hdrs)
+		if err != nil {
+			glog.Error("Could not create multipart part ", err)
+		}
+
+		io.Copy(fw, bytes.NewBuffer(result))
+	}
+	mw.Close()
+
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/users", allUsers).Methods("GET")
-	myRouter.HandleFunc("/user", newUser).Methods("POST")
+	// transcoder
 	myRouter.HandleFunc("/stream", newStream).Methods("POST")
+	myRouter.HandleFunc("/broadcaster", getBroadcaster).Methods("GET")
+	// broadcaster
+	myRouter.HandleFunc("/live/{stream_id}/{media_number}", handlePush).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
-func initialMigration() {
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		fmt.Println(err.Error())
-		panic("failed to connect database")
-	}
-	defer db.Close()
-
-	// Migrate the schema
-	db.AutoMigrate(&User{})
-}
-
 func main() {
-	fmt.Println("Go ORM Tutorial")
-
-	initialMigration()
-	// Handle Subsequent requests
+	flag.StringVar(&broadcastercfg, "broadcaster", "", "broadcaster address")
+	flag.Parse()
+	fmt.Println("Starting Rest API Server")
 	handleRequests()
 }
--- detection/detection.go
+++ detection/detection.go
@@ -2,7 +2,6 @@ package detection
 
 import (
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"net/url"
 	"os"
@@ -74,132 +73,44 @@ func validDnnfilters() []string {
 	return valids
 }
 
-func transcode(hlsStream stream.HLSVideoStream, flagclass int, tinterval float64) (func(*stream.HLSSegment, bool), error) {
-	//Create Transcoder
-	profiles := []ffmpeg.VideoProfile{
-		ffmpeg.P720p25fps16x9,
-		//ffmpeg.PDnnDetector,
-		//ffmpeg.P240p30fps16x9,
-		//ffmpeg.P576p30fps16x9,
-	}
-	workDir := ".tmp/"
-	strmID := hlsStream.GetStreamID()
-	t := transcoder.NewFFMpegSegmentTranscoder(profiles, workDir)
-	//t.SetStreamID(strmID)
-	pid := ffmpeg.AddParallelID(strmID)
-
-	if pid == -1 {
-		glog.Errorf("Can not find transcoding pid with streamid: %v", strmID)
-		return nil, ffmpeg.ErrTranscoderStp
-	}
-
-	t.SetParallelID(pid)
-	glog.Infof("Set PID, strmID: %v %v\n", pid, strmID)
-
-	gpuid := ffmpeg.GetGpuIdx(strmID)
-	if gpuid >= 0 {
-		t.SetGpuID(gpuid)
-	}
-	precontents := ""
-	//load temp warning video(warning.ts)
-	warningbuff, err := ioutil.ReadFile("warning.ts")
-	if err != nil {
-		glog.Errorf("Can not read temp warning video(warning.ts): %v", err)
-	}
-
-	subscriber := func(seg *stream.HLSSegment, eof bool) {
-		//If we get a new video segment for the original HLS stream, do the transcoding.
-		//glog.Infof("Got seg: %v %v\n", seg.Name, hlsStream.GetStreamID())
-		//getfile := ".tmp/" + seg.Name
-		getfile := workDir + seg.Name
-		//Transcode stream
-		tData, contents, err := t.Transcode(getfile)
-		if err != nil {
-			glog.Errorf("Error transcoding: %v", err)
-		} else {
-
-			//Insert into HLS stream
-			//for i, strmID := range strmIDs
-			for i, p := range profiles {
-				if strings.Index(p.Name, "PDnn") >= 0 {
-					continue
-				}
-				//glog.Infof("Inserting transcoded seg %v into strm: %v", len(tData[i]), strmID)
-				sName := fmt.Sprintf("%v_%v.ts", strmID, seg.SeqNo)
-				PgDataTime := false
-				PgDataEnd := false
-				FgContents := 0 //0:Contents None, 1:ContentsStart, 2:ContentsContinue, 3:ContentsEnd
-
-				if len(precontents) == 0 && len(contents) == 0 { //normal
-					//write normal mode
-					FgContents = stream.ContentsNone
-				} else if len(precontents) == 0 && len(contents) > 0 { //started other contents
-					//write EXT-X-PROGRAM-DATE-TIME start
-					//write EXT-X-DATERANGE start
-					PgDataTime = true
-					FgContents = stream.ContentsStart
-
-				} else if len(precontents) > 0 && len(contents) > 0 { //continue other contents
-					//write EXT-X-DATERANGE continue
-					FgContents = stream.ContentsContinue
-
-				} else if len(precontents) > 0 && len(contents) == 0 { //ended other contents
-					//write EXT-X-DISCONTINUITY at end of EXT-X-PROGRAM-DATE-TIME
-					//write EXT-X-DATERANGE end
-					PgDataEnd = true
-					FgContents = stream.ContentsEnd
-				}
-				isYolo := ffmpeg.GetYoloDetectorID()
-				if len(contents) > 0 && isYolo < 0 {
-					glog.Infof("Get Dnn filtering contents at pid %v :%v\n", pid, contents)
-					if err := hlsStream.AddHLSSegment(&stream.HLSSegment{SeqNo: seg.SeqNo, Name: sName, Data: warningbuff,
-						Duration: 2, PgDataTime: PgDataTime, PgDataEnd: PgDataEnd, FgContents: FgContents, ObjectData: contents, IsYolo: isYolo}); err != nil {
-						glog.Errorf("Error writing transcoded seg: %v", err)
-					}
-				} else {
-					if err := hlsStream.AddHLSSegment(&stream.HLSSegment{SeqNo: seg.SeqNo, Name: sName, Data: tData[i],
-						Duration: 2, PgDataTime: PgDataTime, PgDataEnd: PgDataEnd, FgContents: FgContents, ObjectData: contents, IsYolo: isYolo}); err != nil {
-						glog.Errorf("Error writing transcoded seg: %v", err)
-					}
-				}
-			}
-		}
-		precontents = contents
-	}
-
-	return subscriber, nil
-}
-
-func CreateVideoProfile(profiles []ffmpeg.VideoProfile) {
-	// interval := 1.5
-	// metamode := 2
-
-	// flag.Set("logtostderr", "true")
-
+func InitDnnEngine(profiles []ffmpeg.VideoProfile) {
 	dir, err := os.Getwd()
 	if err != nil {
 		glog.Infof("Error getting work directory: %v", err)
 	}
 	glog.Infof("Settig working directory %v", fmt.Sprintf("%v/.tmp", dir))
-	// lpms := core.New(&core.LPMSOpts{WorkDir: fmt.Sprintf("%v/.tmp", dir)})
 
-	//Streams needed for transcoding:
-	// var rtmpStrm stream.RTMPVideoStream
-	// var hlsStrm stream.HLSVideoStream
-	// var manifest stream.HLSVideoManifest
-	// var cancelSeg context.CancelFunc
-
-	//loading dnnmodule only once
-	//ffmpeg.InitDnnEngine(ffmpeg.PDnnDetector)
-	//Register Dnn filter into Transcode Engine
 	cengginflag := true
 	ffmpeg.SetCengineFlag(cengginflag)
 	ffmpeg.SetAvailableGpuNum(1)
 	ffmpeg.SetParallelGpuNum(1)
 
 	for _, ft := range profiles {
+		ft.Detector.Interval = 1.0 //need profile from config
+		ft.Detector.MetaMode = 2   //recommand HLS metatag mode
 		glog.Infof("Registry DnnEngine: name: %v metadamode: %v", ft.Name, ft.Detector.MetaMode)
 		ffmpeg.RegistryDnnEngine(ft)
 	}
 
+}
+
+func ProcessSegment(seg *stream.HLSSegment, strmID string, profiles []ffmpeg.VideoProfile) ([][]byte, string, error) {
+	workDir := ".tmp/"
+
+	t := transcoder.NewFFMpegSegmentTranscoder(profiles, workDir)
+	pid := ffmpeg.AddParallelID(strmID)
+
+	if pid == -1 {
+		glog.Errorf("Can not find transcoding pid with streamid: %v", strmID)
+		// return nil
+	}
+	t.SetParallelID(pid)
+	glog.Infof("Set PID, strmID: %v %v\n", pid, strmID)
+	gpuid := ffmpeg.GetGpuIdx(strmID)
+	if gpuid >= 0 {
+		t.SetGpuID(gpuid)
+	}
+
+	getfile := workDir + seg.Name
+	return t.Transcode(getfile)
 }
